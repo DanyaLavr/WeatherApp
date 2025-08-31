31775a0be8bc8cde3767b6956f67a80ed60520d0
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2025-09-01 00:50:13 +0200

    add rule for render of chart

--- src/views/statistic/Statistic.jsx
+++ src/views/statistic/Statistic.jsx
@@ -12,38 +12,57 @@ import {
 import { createBem } from '@/utils/createBem';
 
 import styles from './statistic.module.scss';
+import Loader from '@/components/loader/Loader';
+import CustomTooltip from './CustomTooltip';
 const bem = createBem('statistic', styles);
 
+const chartHeight = window.innerWidth <= 480 ? 250 : window.innerWidth <= 768 ? 300 : 400;
+
+const formatDateTime = (str) => {
+  const date = new Date(str);
+  const hours = date.getHours();
+
+  if (hours === 0) {
+    const day = date.getDate();
+    const month = date.toLocaleString('en-US', { month: 'short' });
+    return `${day} ${month}`;
+  }
+  if (hours > 12) return `${(hours - 12).toString()} pm`;
+
+  return `${hours.toString()} am`;
+};
 const Statistic = () => {
   const [data, setData] = useState([]);
   const [error, setError] = useState('');
 
-  const chartHeight = window.innerWidth <= 480 ? 250 : window.innerWidth <= 768 ? 300 : 400;
-
   useEffect(() => {
     const apiKey = 'fcc3ac1b514a2bdf8898cc0061159146';
-    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=50&lon=50&units=metric&appid=${apiKey}`;
+    const city = 'Alicante';
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
 
     const getWeather = async () => {
       try {
         const response = await axios.get(url);
-        console.log(response.data);
-        const hourlyData = response.data.list.slice(0, 20).map((item) => {
-          const date = new Date(item.dt * 1000);
-          const hours = date.getHours();
-          const suffix = hours >= 12 ? 'pm' : 'am';
-          const formattedHour = `${hours % 12 || 12} ${suffix}`;
-
+        const weatherList = response.data.list;
+        const renderArr = weatherList.map((elem) => {
+          const weatherTime = formatDateTime(elem.dt_txt);
           return {
-            time: formattedHour,
-            temp: item.main.temp.toFixed(1),
-            feels_like: item.main.feels_like.toFixed(1),
-
-            description: item.weather[0].description,
+            time: weatherTime,
+            temp: elem.main.temp.toFixed(1),
+            feels_like: elem.main.feels_like.toFixed(1),
+            icon: elem.weather[0].icon,
+            description: elem.weather[0].description,
           };
         });
-
-        setData(hourlyData);
+        if (window.innerWidth <= 1439 && window.innerWidth >= 834) {
+          setData(renderArr.slice(0, 8));
+        } else if (window.innerWidth <= 833) {
+          setData(
+            renderArr.slice(0, 10).filter((elem, idx) => {
+              if (idx % 4 === 0) return elem;
+            })
+          );
+        } else setData(renderArr);
       } catch (err) {
         console.error(err);
         setError('Не вдалося завантажити погоду.');
@@ -51,29 +70,6 @@ const Statistic = () => {
     };
     getWeather();
   }, []);
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
-  const CustomTooltip = ({ active, payload, label }) => {
-    if (active && payload && payload.length) {
-      const { temp, feels_like, icon, description } = payload[0].payload;
-      return (
-        <div
-          className="custom-tooltip"
-          style={{ background: '#fff', border: '1px solid #ccc', padding: 10 }}
-        >
-          <p>
-            <strong>{label}</strong>
-          </p>
-          <img src={icon} alt={description} width="50" />
-          <p>Температура: {temp}°C</p>
-          <p>Відчувається як: {feels_like}°C</p>
-          <p style={{ textTransform: 'capitalize' }}>{description}</p>
-        </div>
-      );
-    }
-    return null;
-  };
 
   return (
     <div className={`container ${bem('container')}`}>
@@ -90,14 +86,11 @@ const Statistic = () => {
               orientation="top"
               axisLine={false}
               tickLine={false}
-              tick={({ x, y, payload }) => {
-                const showTick = window.innerWidth > 768 || payload.index % 2 === 0;
-                return showTick ? (
-                  <text x={x} y={y - 10} textAnchor="middle" fill="#333" fontSize="12">
-                    {payload.value}
-                  </text>
-                ) : null;
-              }}
+              tick={({ x, y, payload }) => (
+                <text x={x} y={y - 10} textAnchor="middle" fill="#333" fontSize="10">
+                  {payload.value}
+                </text>
+              )}
             />
             <YAxis
               axisLine={false}
@@ -105,13 +98,14 @@ const Statistic = () => {
               domain={[5, 35]}
               ticks={[5, 10, 15, 20, 25, 30, 35]}
               unit="°C"
+              fontSize="10"
             />
             <Tooltip content={<CustomTooltip />} />
             <Line type="monotone" dataKey="temp" stroke="#FFA500" strokeWidth={2} dot={false} />
           </LineChart>
         </ResponsiveContainer>
       ) : (
-        <p>Завантаження погоди...</p>
+        <Loader />
       )}
     </div>
   );
