fa1950dda60ad5853f0bfbce2af51ff722bddd90
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2025-09-01 13:24:03 +0200

    delete button , when is error on News.jsx

--- src/views/news/News.jsx
+++ src/views/news/News.jsx
@@ -40,18 +40,19 @@ const News = () => {
       <div className="container">
         <h1 className={bem('title')}>Interacting with our pets</h1>
         {news.length !== 0 && (
-          <ul className={bem('list')}>
-            {news.map((item, index) => (
-              <NewsItem title={item.title} image={item.urlToImage} url={item.url} key={index} />
-            ))}
-          </ul>
+          <>
+            <ul className={bem('list')}>
+              {news.map((item, index) => (
+                <NewsItem title={item.title} image={item.urlToImage} url={item.url} key={index} />
+              ))}
+            </ul>
+            <button type="button" className={bem('button')} onClick={handleIncrement}>
+              See more
+            </button>
+          </>
         )}
         {isLoading && <Loader />}
         {error && error}
-
-        <button type="button" className={bem('button')} onClick={handleIncrement}>
-          See more
-        </button>
       </div>
     </section>
   );
